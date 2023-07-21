d64affd5671fb9fac422c91e4e7067d5e1f35da7
Xiawei <xiaweiss@foxmail.com>
2023-07-21 14:33:21 +0800

    bug: #129

--- project/miniprogram/pages/home/index.ts
+++ project/miniprogram/pages/home/index.ts
@@ -1,7 +1,20 @@
+import { wxToPromise } from '../../utils/index'
+
 Page({
   data: {
     /** 触发下拉刷新 */
     refresherTriggered: false,
+    url: ''
+  },
+  async onLoad () {
+    // （使用本地路径，避免多次请求网络）
+    const [res] = await wxToPromise(wx.getImageInfo, {src: 'https://xiaweiss.oss-cn-beijing.aliyuncs.com/backdrop_img.png'})
+
+    if (res) {
+      this.setData({
+        url: res.path
+      })
+    }
   },
   onTap () {
 
